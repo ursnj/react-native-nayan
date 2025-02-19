bbfe397eb7dd390086bc4e249a8a814de8fc49b2
ursnj <niranjan.devasani@gmail.com>
2025-02-19 15:17:36 +0530

    Setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,12 @@
 import '../global.css';
 import { NText } from 'react-native-nayan';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <NText />
-      <Text className="text-green-500">Text</Text>
+      <Text className="text-green-500 text-xl mt-3">Text</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
