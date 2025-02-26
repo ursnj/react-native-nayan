91e74e5263d42dd07f4c34feb60e8a514c675404
ursnj <niranjan.devasani@gmail.com>
2025-02-26 10:03:48 +0530

    Setup

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    'react-native-builder-bob/babel-preset',
-    [
-      '@babel/preset-react',
-      { runtime: 'automatic', importSource: 'nativewind' },
-    ],
-  ],
-};
--- src/components/NText.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from 'react-native';
-
-export const NText = (props: any) => {
-  return <Text className={'text-red-500 ' + props.className}>Sample</Text>;
-};
