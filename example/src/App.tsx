072fe0d65e6ea62b08dc10b39f819c34701b5e21
ndevasan <ndevasan@cisco.com>
2025-03-07 17:10:19 +0530

    Lib code working

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,13 @@
 import { NText } from 'react-native-nayan';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center">
       <NText />
+      <Pressable onPress={() => console.log('Pressed')}>
       <Text className="text-red-500 text-xl mt-3">Text</Text>
+      </Pressable>
     </View>
   );
 }
