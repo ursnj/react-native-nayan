0c3a5e4c61a817176ca546c3ecca49b893448849
ndevasan <ndevasan@cisco.com>
2025-03-07 17:10:39 +0530

    Lib code working

--- src/components/NText.tsx
+++ src/components/NText.tsx
@@ -1,5 +1,5 @@
 import { Text } from 'react-native';
 
 export const NText = (props: any) => {
-  return <Text className={'text-blue-500 p-5 bg-red-200 rounded-full ' + props.className}>Sample</Text>;
+  return <Text className={'text-blue-500 ' + props.className}>Sample</Text>;
 };
