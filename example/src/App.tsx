78b5dd8296c923c6e9ed3a4b88a87201fd8f0b7c
ursnj <niranjan.devasani@gmail.com>
2025-03-09 00:59:25 +0530

    Setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,17 @@
 import '../global.css';
 import 'react-native-reanimated';
-import { NText, NPress } from 'react-native-nayan';
+import { NText, NPress, NTheme } from 'react-native-nayan';
 import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <NText className="text-green-500">Sample</NText>
-      <NPress onPress={() => console.log('Pressed')}>
-        <NText className="text-red-500 text-xl mt-3">Text</NText>
-      </NPress>
-    </View>
+    <NTheme theme="light" themeColors={{}}>
+      <View className="flex-1 justify-center items-center">
+        <NText className="text-green-500">Sample</NText>
+        <NPress onPress={() => console.log('Pressed')}>
+          <NText className="text-red-500 text-xl mt-3">Text</NText>
+        </NPress>
+      </View>
+    </NTheme>
   );
 }
