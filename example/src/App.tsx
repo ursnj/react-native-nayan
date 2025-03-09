7b5856376a1fd19865f06372f060d870e3efe980
ursnj <niranjan.devasani@gmail.com>
2025-03-09 09:34:36 +0530

    Setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,22 @@
 import '../global.css';
 import 'react-native-reanimated';
-import { NText, NPress, NTheme } from 'react-native-nayan';
+import { NText, NPress, NTheme, useColorScheme } from 'react-native-nayan';
 import { View } from 'react-native';
+import { themeColors } from './constants';
 
 export default function App() {
+  const {colorScheme, setColorScheme} = useColorScheme();
+
+  const chnageTheme = () => {
+    setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
+  }
+
   return (
-    <NTheme theme="light" themeColors={{}}>
+    <NTheme theme="light" themeColors={themeColors}>
       <View className="flex-1 justify-center items-center">
         <NText className="text-green-500">Sample</NText>
-        <NPress onPress={() => console.log('Pressed')}>
-          <NText className="text-red-500 text-xl mt-3">Text</NText>
+        <NPress onPress={chnageTheme}>
+          <NText className="text-red-500 text-xl mt-3">Change Theme</NText>
         </NPress>
       </View>
     </NTheme>
