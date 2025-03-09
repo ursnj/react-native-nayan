3687bebb4f24859cce27d9f5245a4123f6b554a8
ursnj <niranjan.devasani@gmail.com>
2025-03-09 09:37:36 +0530

    Setup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,9 +14,8 @@ export default function App() {
   return (
     <NTheme theme="light" themeColors={themeColors}>
       <View className="flex-1 justify-center items-center">
-        <NText className="text-green-500">Sample</NText>
         <NPress onPress={chnageTheme}>
-          <NText className="text-red-500 text-xl mt-3">Change Theme</NText>
+          <NText className="text-text text-xl mt-3">Change Theme</NText>
         </NPress>
       </View>
     </NTheme>
--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,3 +1,6 @@
+const { hairlineWidth } = require('nativewind/theme');
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -7,7 +10,34 @@ module.exports = {
   ],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      primary: 'var(--color-primary)',
+      card: 'var(--color-card)',
+      text: 'var(--color-text)',
+      muted: 'var(--color-muted)',
+      border: 'var(--color-border)',
+      background: 'var(--color-background)'
+    },
+    extend: {
+      borderWidth: {
+        hairline: hairlineWidth()
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
