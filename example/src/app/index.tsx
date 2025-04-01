45daa86c9460a81e5c6f2bb851fe4ced0cd00089
ndevasan <ndevasan@cisco.com>
2025-04-01 17:00:22 +0530

    Add components to the example

--- example/src/app/index.tsx
+++ example/src/app/index.tsx
@@ -1,7 +1,10 @@
-import { View } from 'react-native';
-import { NButton, useColorScheme, THEMES } from 'react-native-nayan';
+import { Stack, useRouter } from 'expo-router';
+import { ScrollView } from 'react-native';
+import { NPress, NText, useColorScheme, THEMES } from 'react-native-nayan';
+import { components } from '../constants';
 
 export default function Home() {
+  const router = useRouter();
   const {isDarkColorScheme, setColorScheme} = useColorScheme();
 
   const changeTheme = () => {
@@ -9,8 +12,15 @@ export default function Home() {
   }
 
   return (
-      <View className="flex-1 justify-center items-center bg-background">
-        <NButton onPress={changeTheme}>Change Theme</NButton>
-      </View>
+      <ScrollView className="flex-1 bg-background pb-3">
+        <Stack.Screen options={{headerRight: () => {
+          return (
+            <NPress onPress={changeTheme} className="p-3">
+              <NText>{isDarkColorScheme ? 'Light' : 'Dark'}</NText>
+            </NPress>
+          );
+          }}} />
+        {components.map((component: any) => <NPress key={component.name} className="py-3 px-4 bg-card m-3 mb-0 border border-border rounded" onPress={() => router.navigate({ pathname: component.name })}><NText>{component.title}</NText></NPress>)}
+      </ScrollView>
   );
 }
